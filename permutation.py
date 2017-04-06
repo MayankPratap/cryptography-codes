92825af3ab17f212299ca7600ea6fde878bbfeba
Mayank Pratap Singh <MayankPratap@users.noreply.github.com>
2017-04-06 23:58:36 +0530

    Removed bug by changing Matrix2[i][key[j]]=Matrix2[i][j] to Matrix2[i][j]=Matrix[i][key[j]] in line 36

--- permutation.py
+++ permutation.py
@@ -1,23 +1,21 @@
-# Keyed transpose -> Permutation Cipher
+# Keyed transpose -> Permutation cipher
 import math
 plain=raw_input("Enter plaintext: ")
 cols=int(raw_input("Enter number of columns: "))
 
 key=['x' for x in range(cols)]
-
 keydict={}
 
 print "Enter keys: "
 
 for i in range(cols):
-    x=int(raw_input(''))
-    key[i]=x-1
-    keydict[x-1]=i
+    x=int(raw_input())
+    key[i]=x
+    keydict[x]=i
 
 rows=int(math.ceil(len(plain)*1.0/cols))
 
 Matrix=[['x' for x in range(cols)] for y in range(rows)]
-
 Matrix2=[['x' for x in range(cols)] for y in range(rows)]
 
 i=0
@@ -31,40 +29,24 @@ for k in plain:
     j=j+1
 
 print "Plaintext in matrix form: "
+print Matrix
 
 for i in range(rows):
     for j in range(cols):
-        print Matrix[i][j],
-
-    print
-
-for i in range(rows):
-    for j in range(cols):
-        Matrix2[i][key[j]]=Matrix[i][j]
+        Matrix2[i][j]=Matrix[i][key[j]]
 
 print "Ciphertext in matrix form: "
+print Matrix2
 
-for i in range(rows):
-    for j in range(cols):
-        print Matrix2[i][j],
-    print
-
-Matrix3=[['x' for x  in range(cols)] for y in range(rows)]
-
-
+Matrix3=[['x' for x in range(cols)] for y in range(rows)]
 
 for i in range(rows):
     for j in range(cols):
         Matrix3[i][j]=Matrix2[i][keydict[j]]
 
-
 print "Deciphered matrix: "
 
-for i in range(rows):
-    for j in range(cols):
-        print Matrix3[i][j],
-
-    print
+print Matrix3
 
 decipher=""
 
@@ -73,5 +55,4 @@ for i in range(rows):
         decipher+=Matrix3[i][j]
 
 print "Deciphered text: "
-
 print decipher
